e262fbcd41bdd51d9777a0f6108435b203f8ad9b
Gui Heurich <guilherme.heurich@protonmail.com>
2022-07-05 11:13:05 +0100

    add basic styling

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "GET /index" do
+    it "renders" do
+      get "/home"
+      expect(response).to render_template("home/index")
+    end
+  end
+end
